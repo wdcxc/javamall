5400d3b13f3861c149915282b00cae4dab695f9f
xcchen <904342189@qq.com>
2019-04-30 14:21:23 +0800

    project init

---
+++ src/main/java/com/javamall/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.javamall.dao;
+
+import com.javamall.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
